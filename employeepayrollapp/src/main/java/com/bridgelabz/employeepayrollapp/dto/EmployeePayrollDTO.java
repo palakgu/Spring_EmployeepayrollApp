d3d3bc4e9de236326c2e1d9806ee5e69a2d60617
palakgu <palakjv304@gmail.com>
2025-02-28 15:07:43 +0530

    [Palak] add. Employee payroll app Ensure validations is done on the Payroll DTO

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,29 +1,36 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Max;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.ToString;
 
 import java.time.LocalDate;
 import java.util.List;
-@Data
+
 @ToString
 public class EmployeePayrollDTO {
 
-    @NotEmpty(message = "Name cannot be empty")
-    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee name Invalid")
     public String name;
 
-    @Min(value = 1000, message = "Salary must be at least 1000")
-    @Max(value = 1000000, message = "Salary cannot exceed 1,000,000")
+    @Min(value = 500, message = "Min Wage should be more than 500")
     public int salary;
-    public String gender;
-    public LocalDate startDate;
-    public String note;
-    public String profilePic;
-    public List<String> department;
 
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
+    public String gender;
+
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "startDate should Not be Empty")
+    @PastOrPresent(message = "startDate should be past or today's date")
+    public LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be Empty")
+    public String note;
+
+    @NotBlank(message = "profilePic cannot be Empty")
+    public String profilePic;
+
+    @NotNull(message = "department should Not be Empty")
+    public List<String> department;
 }
